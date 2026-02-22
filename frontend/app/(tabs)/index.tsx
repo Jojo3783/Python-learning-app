ceba71c07b136f0445e6d759fac51740eebfffcd
Joe <abc0912112716@gmail.com>
2026-02-22 12:47:51 +0000

    1st

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,36 @@
+import { Background } from "@react-navigation/elements";
+import React, {useState} from "react";
+import {Text, Button, View} from "react-native";
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const HomePage = () => {//enter game 
+  return (
+    <View>
+      <Text style={styles.text}>Python學習應用程式</Text>
+      <Button 
+      title="進入遊戲"
+      onPress={SelectLevelPage}
+      >
+
+      </Button>
+    </View>
+  );
+};
+const SelectLevelPage = () => {//select level
+  
+};
+
+const GamePage = () => {//play game
+
+};
+const main = () => {
+
+};
+
+const styles = ({
+  text : {
+  
+  }
+})
+
+export default HomePage;
