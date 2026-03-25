61c166ebc54dc57a3eaaee07ef704766f9b882a8
FUNDAI <ethan@jaiohsieh.com>
2026-03-25 21:39:51 +0800

    修正requirements，新增要求資料表，變更連結方式

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,19 @@
+# backend/database.py
+import os
 from sqlalchemy import create_engine
 from typing import Annotated
-from sqlalchemy.orm import sessionmaker, declarative_base , Session
+from sqlalchemy.orm import declarative_base , Session
 from fastapi import Depends
+from dotenv import load_dotenv
 
 # 不要問我///在幹嘛，官方講的，後面指的是在這個資料夾裡的classroom.db
-db_url = "sqlite:///./classroom.db" 
+load_dotenv()
 # 與資料庫連線，把同一線程驗證關掉
-engine = create_engine(db_url , connect_args={"check_same_thread": False})
+db_url = os.getenv("DATABASE_URL")
 
 Base = declarative_base()
+
+engine = create_engine(db_url)
 
 def get_session():
   with Session(engine) as session:
